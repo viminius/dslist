0a3709b711f7446a762db8e40cae253d4333413e
Vinícius <viniciusmoraesdasilva25@gmail.com>
2024-11-12 15:35:21 -0300

    Game repository, service, controller

---
+++ src/main/java/com/pessoal/dslist/repositories/GameRepository.java
@@ -0,0 +1,10 @@
+package com.pessoal.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pessoal.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+	
+}
